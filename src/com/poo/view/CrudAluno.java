56a8c1bbfc86d8ae434e7b1c6606a2b0e6a4e93e
Mona-Lee Braga <41974988+Mona-Lee12@users.noreply.github.com>
2020-06-10 12:33:11 -0300

    ajustes-template

--- src/com/poo/view/CrudAluno.java
+++ src/com/poo/view/CrudAluno.java
@@ -13,6 +13,9 @@ import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import com.poo.controller.AlunoController;
 import com.poo.model.Aluno;
@@ -175,5 +178,38 @@ public class CrudAluno extends JFrame {
     public static void main(String[] args) {
         CrudAluno lc = new CrudAluno();
         lc.setVisible(true);
+    }
+	
+    public static class AplicaNimbusLookAndFeel {
+   	 
+     public static void pegaNimbus() {
+    	    try {
+    	        for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+    	            if ("Nimbus".equals(info.getName())) {
+    	                UIManager.setLookAndFeel(info.getClassName());
+    	                break;
+    	            }
+    	        }
+    	    } catch (UnsupportedLookAndFeelException e) {
+    	         
+    	        System.out.println("Erro: " + e.getMessage());
+    	        e.printStackTrace();
+    	         
+    	    } catch (ClassNotFoundException e) {
+    	         
+    	        System.out.println("Erro: " + e.getMessage());
+    	        e.printStackTrace();
+    	         
+    	    } catch (InstantiationException e) {
+    	         
+    	        System.out.println("Erro: " + e.getMessage());
+    	        e.printStackTrace();
+    	         
+    	    } catch (IllegalAccessException e) {
+    	         
+    	        System.out.println("Erro: " + e.getMessage());
+    	        e.printStackTrace();
+    	    }
+    	}
     }
 }
